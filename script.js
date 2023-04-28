a53e004049333c9850e8d1fb41eb4305b2d8f41c
william hill <williamhill0831@Hotmail.com>
2023-04-28 15:45:49 -0400

    working on book object

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-let myLibrary = [];
+const books = document.querySelectorAll('.book');
+console.log(books);
+
+let log = console.log;
+
+let myLibrary = [
+  {
+    book: 'Rich Dad Poor Dad',
+    author: 'Robert Kiyosaki',
+    pages: '300',
+    read: 'read',
+  },
+  {
+    book: 'The Slight Edge',
+    author: 'Jeff Olson',
+    pages: '230',
+    read: 'read',
+  },
+];
 
 function Book() {
   // the constructor...
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  myLibrary.forEach((book) => {
+    books[0].textContent = book;
+    books[1].innerText = book;
+    log(books[0].id);
+    // console.log(books, 'books');
+    // console.log(book, 'book');
+  });
 }
+
+addBookToLibrary();
