d410826878c5c8fd4e99ca4f69f088fdd67227b5
William Hill <williamhill@Williams-MacBook-Pro.local>
2023-05-01 02:49:45 -0400

    Finished setting up starting library books

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const firstBookTitle = document.querySelector('.titleOne');
-
 let log = console.log;
+log(addNewBookButton);
 
-const book1 = {
-  book: 'Rich Dad Poor Dad',
-  author: 'Robert Kiyosaki',
-  pages: '300',
-  read: 'read',
-};
+function bookFrom() {}
 
-const book2 = {
-  book: 'The Slight Edge',
-  author: 'Jeff Olson',
-  pages: '230',
-  read: 'read',
-};
+// const book1 = {
+//   book: 'Rich Dad Poor Dad',
+//   author: 'Robert Kiyosaki',
+//   pages: '300',
+//   read: 'read',
+// };
+
+// const book2 = {
+//   book: 'The Slight Edge',
+//   author: 'Jeff Olson',
+//   pages: '230',
+//   read: 'read',
+// };
 
 let myLibrary = [];
 
@@ -22,35 +23,6 @@ function Book() {
   // the constructor...
 }
 
-function startingBooks() {
-  const title = book1.book;
-  const ok = document.createTextNode(book1.author);
-  firstBookTitle.appendChild(ok);
-  const author = book1.author;
-  const pages = book1.pages;
-  let read = book1.read;
+function addBookToLibrary() {}
 
-  log(title);
-}
-
-// function addBookToLibrary() {
-//   const keys = Object.keys(myLibrary);
-//   keys.forEach((key) => {
-//     console.log(myLibrary[key]);
-//     const bookInfo = document.createTextNode(myLibrary[key]);
-//     firstBook.appendChild(bookInfo);
-//   });
-// }
-
-// function addBookToLibrary() {
-//   myLibrary.forEach((bookDetails) => {
-
-//     const bookInfo = document.createTextNode(bookDetails.book);
-//     firstBook.appendChild(bookInfo);
-//     // log(bookDetails);
-//   });
-// }
-
-// addBookToLibrary();
-
-startingBooks();
+addBookToLibrary();
