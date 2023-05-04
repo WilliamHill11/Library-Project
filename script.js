cf825c7c11e22628c619b21bc15026a3a918da40
william hill <williamhill0831@Hotmail.com>
2023-05-04 00:17:24 -0400

    stored user input into an object/constructor

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const formSubmit = document.getElementById('formSubmit');
 const openButton = document.querySelector('[data-button-target]');
 const closeButton = document.querySelector('[data-close-button]');
 const overlay = document.getElementById('overlay');
+const libraryWrapper = document.querySelector('.library');
 
 openButton.addEventListener('click', () => {
   const modal = document.querySelector(openButton.dataset.buttonTarget);
@@ -35,7 +36,7 @@ closeBook.addEventListener('click', () => {
 function bookForm() {
   form.style.display = 'block';
 }
-
+// e.stopImmediatePropagation();
 //Prevent form Submit
 formSubmit.addEventListener('click', (e) => {
   if (form.checkValidity()) {
@@ -47,8 +48,6 @@ formSubmit.addEventListener('click', (e) => {
   }
 });
 
-function resetBook() {}
-
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -59,7 +58,24 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  const title = new Book(this.title);
+  let newBook = new Book(
+    document.querySelector("input[name='bookTitle']").value,
+    document.querySelector("input[name='author']").value,
+    document.querySelector("input[name='pages']").value,
+    document.querySelector("input[name='read']").value
+  );
+  myLibrary.push(newBook);
+  form.reset();
+  Object.values(myLibrary).forEach((value) => {
+    const book = document.createElement('div');
+    libraryWrapper.appendChild(book);
+    book.classList.add('book', 'newBookTitle', 'newBookContent');
+    book.innerHTML = value.title;
+    console.log(value.title);
+    console.log(value.author);
+    console.log(value.pages);
+    console.log(value.read);
+  });
 }
 
 // const book1 = {
